7cc29fdeb7e1358353af08f3e5e18d24f3cfad43
Hamburboy <alonhamb@gmail.com>
2025-11-15 16:21:53 +0200

    updated base from dev

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.kt
@@ -2,5 +2,15 @@ package org.firstinspires.ftc.teamcode
 
 object RobotMap {
 
+    object Shooter {
 
+        val HOOD_SERVO_ID = "hood servo"
+
+        val TURRET_ENCODER_ID = "turret encoder"
+
+        val TURRET_SERVO_ID = "turret servo"
+
+        val FLYWHEEL_MOTOR_ID = "flywheel motor"
+
+    }
 }
